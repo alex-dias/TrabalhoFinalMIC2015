acb55b7cf3f72b86afc5ba43be2f61170337fc4d
Igor Felipe Gallon <igor.felipe.gallon@outlook.com>
2015-11-27 17:50:30 -0200

    Programa Inicial
    Testes iniciais para interrupção do motor e LCD. Foi encontrado problemas para fazer o motor voltar a rodar.

---
+++ program.ino
@@ -0,0 +1,47 @@
+#include <Wire.h>
+#include <Stepper.h>
+#include <rgb_lcd.h>
+#define STEPS 200
+
+Stepper stepper(STEPS,8,10,11,9); 
+rgb_lcd lcd;
+int button = 2;
+volatile int flag = 1;
+
+void setup() {
+//  pinMode(button, INPUT);
+  attachInterrupt(0, parae, RISING);
+  //stepper.setSpeed(50);
+  lcd.begin(16, 2);
+
+}
+
+void loop() {
+  stepper.step(-200);
+  
+  stepper.setSpeed(flag*50);  
+  
+//  if(!flag)
+//      lcd.print("Esteiras GENOMAX");
+//  else
+//      lcd.print("ESTEIRA PARADA");
+
+}
+
+
+//void parae(){
+//  if(flag == 0){
+//    lcd.print("ESTEIRA VAZIA");
+//    stepper.setSpeed(0);
+//    flag = 1;
+//  }
+//   else{
+//     stepper.setSpeed(50);
+//     lcd.print("Esteiras GENOMAX");
+//     flag = 0;
+//  }
+//}
+
+void parae(){
+  flag = !flag;
+}
