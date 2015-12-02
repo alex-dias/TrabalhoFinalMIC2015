98d3448c0a57e3c783ffde480b77fdac7885ff48
Igor Felipe Gallon <igor.felipe.gallon@outlook.com>
2015-12-02 17:01:47 -0200

    Versão com o botão on/off
    Esteira funcionando com o botão on/off no sistema todo.
    Cogitando nova implementação com Servomotor.

--- program.ino
+++ program.ino
@@ -1,3 +1,19 @@
+/*
+======================================================
+                   ESTEIRA GENOMAX
+
+Esteira com sensor de luz que detecta a peça passando.
+Quando  nenhuma  peça  passa  pelo  sensor durante  5
+segundos, a esteira para e é acionado um buzzer  como
+alerta. Além disso, um visor LCD mostra o status atual
+da esteira.
+
+Autores:
+Alexandre Negretti      558214
+Igor Gallon             558320
+======================================================
+*/
+
 #include <Time.h>  
 #include <Wire.h>
 #include <Stepper.h>
@@ -10,54 +26,74 @@ int ledR = 3;
 int sensorLUZ = 0;
 int buzzer = 4;
 volatile int flag = 1;
+volatile int on = 1;
 int leitura = 0;
 time_t t;
 
 void setup() {
-  attachInterrupt(0, parae, RISING);
+  attachInterrupt(0, onOff, RISING);
   pinMode(ledR, OUTPUT);
   pinMode(sensorLUZ, INPUT);
   pinMode(buzzer, OUTPUT);
   lcd.begin(16, 2);
   stepper.setSpeed(50);
   Serial.begin(9600);
-  digitalWrite(ledR, HIGH);
-  setTime(0); 
+  lcd.setCursor(0,1);
+  lcd.print("Esteira GENOMAX");
+  lcd.setCursor(0,0);
+  digitalWrite(ledR,HIGH);
+  setTime(0);
 }
 
 void loop() {
-  
-  t = now();
+  if(on){
+      t = now();
+        
+      if(t > 5)
+        flag = 0;
+      else
+        flag = 1;
     
-  if(t > 5)
-    flag = 0;
-  else
-    flag = 1;
-    
-  leitura = analogRead(sensorLUZ);
-    
-  if(leitura < 600){
-    setTime(0);}
-  
-  if(flag == 0)
-    digitalWrite(buzzer, HIGH);
-  else
-    digitalWrite(buzzer, LOW);
-
-  stepper.step(-200*flag);
-  
-    
-  Serial.println(t);
-//stepper.setSpeed(50);  
-
-//  if(!flag)
-//      lcd.print("Esteiras GENOMAX");
-//  else
-//      lcd.print("ESTEIRA PARADA");
-
+      leitura = analogRead(sensorLUZ);
+      
+      if(leitura < 600){
+        setTime(0);}
+      
+      if(flag == 0)
+        digitalWrite(buzzer, HIGH);
+      else
+        digitalWrite(buzzer, LOW);
+      
+      tela();      
+    }
+    else{
+      lcd.setRGB(0,0,0);
+      lcd.clear();
+      flag=0;
+    }
+    stepper.step(-200*flag);
 }
 
-void parae(){
-  flag = 0;
-  digitalWrite(ledR, LOW);
+void tela(){
+  if(!flag){
+    lcd.setRGB(100,0,0);
+    lcd.print("Inserir peca!   ");
+  }
+  else{
+    lcd.setRGB(255,255,255);
+    lcd.print("Working well...");
+  }
+  lcd.setCursor(0,1);
+  lcd.print("Esteira GENOMAX");
+  lcd.setCursor(0,0);
+}
+
+void onOff(){
+  on = !on;
+  if(!on){
+    digitalWrite(ledR,LOW);
+  }
+  else{
+      digitalWrite(ledR,HIGH);
+    } 
 }
