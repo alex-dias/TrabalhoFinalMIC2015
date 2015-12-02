cf4a127fa1c92bf6f1bf6ea2c62263c9c363539b
alex-dias <alex.d.negretti@gmail.com>
2015-12-02 15:50:58 -0200

    Sensor de luz funcional
    O sensor de luz faz a esteira parar caso ele não detecte nenhum objeto na esteira GENOMAX durante 5 segundos, um buzzer é acionado para a avisar sobre o ocorrido. A próxima etapa é a implementação do visor LCD.

--- program.ino
+++ program.ino
@@ -1,3 +1,4 @@
+#include <Time.h>  
 #include <Wire.h>
 #include <Stepper.h>
 #include <rgb_lcd.h>
@@ -5,24 +6,48 @@
 
 Stepper stepper(STEPS,8,10,11,9); 
 rgb_lcd lcd;
-int button = 2;
+int ledR = 3;
+int sensorLUZ = 0;
+int buzzer = 4;
 volatile int flag = 1;
+int leitura = 0;
+time_t t;
 
 void setup() {
-//  pinMode(button, INPUT);
   attachInterrupt(0, parae, RISING);
-  //stepper.setSpeed(50);
+  pinMode(ledR, OUTPUT);
+  pinMode(sensorLUZ, INPUT);
+  pinMode(buzzer, OUTPUT);
   lcd.begin(16, 2);
   stepper.setSpeed(50);
   Serial.begin(9600);
-
+  digitalWrite(ledR, HIGH);
+  setTime(0); 
 }
 
 void loop() {
+  
+  t = now();
+    
+  if(t > 5)
+    flag = 0;
+  else
+    flag = 1;
+    
+  leitura = analogRead(sensorLUZ);
+    
+  if(leitura < 600){
+    setTime(0);}
+  
+  if(flag == 0)
+    digitalWrite(buzzer, HIGH);
+  else
+    digitalWrite(buzzer, LOW);
+
   stepper.step(-200*flag);
   
     
-  Serial.println(flag);
+  Serial.println(t);
 //stepper.setSpeed(50);  
 
 //  if(!flag)
@@ -32,20 +57,7 @@ void loop() {
 
 }
 
-
-//void parae(){
-//  if(flag == 0){
-//    lcd.print("ESTEIRA VAZIA");
-//    stepper.setSpeed(0);
-//    flag = 1;
-//  }
-//   else{
-//     stepper.setSpeed(50);
-//     lcd.print("Esteiras GENOMAX");
-//     flag = 0;
-//  }
-//}
-
 void parae(){
-  flag = !flag;
+  flag = 0;
+  digitalWrite(ledR, LOW);
 }
