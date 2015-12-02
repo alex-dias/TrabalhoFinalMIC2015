170e1f0b5c5da4ce32da6800bc9d632914f0a4d8
Igor Felipe Gallon <igor.felipe.gallon@outlook.com>
2015-12-02 17:37:18 -0200

    Adição do servomotor
    O servomotor empurra a peça da esteira depois que passa pelo sensor. Os erros das versões anteriores foram corrigidos.

--- program.ino
+++ program.ino
@@ -18,17 +18,20 @@ Igor Gallon             558320
 #include <Wire.h>
 #include <Stepper.h>
 #include <rgb_lcd.h>
+#include <Servo.h>
 #define STEPS 200
 
 Stepper stepper(STEPS,8,10,11,9); 
 rgb_lcd lcd;
-int ledR = 3;
+int ledR = 6;
 int sensorLUZ = 0;
-int buzzer = 4;
+int buzzer = 3;
+int servoMotor = 5;
 volatile int flag = 1;
 volatile int on = 1;
 int leitura = 0;
 time_t t;
+Servo servo;
 
 void setup() {
   attachInterrupt(0, onOff, RISING);
@@ -43,12 +46,19 @@ void setup() {
   lcd.setCursor(0,0);
   digitalWrite(ledR,HIGH);
   setTime(0);
+  servo.attach(servoMotor);
+  servo.write(0);
 }
 
 void loop() {
   if(on){
       t = now();
-        
+      
+      if(t > 1)
+        empurra();
+      if(t > 2)
+        volta();
+      
       if(t > 5)
         flag = 0;
       else
@@ -57,12 +67,17 @@ void loop() {
       leitura = analogRead(sensorLUZ);
       
       if(leitura < 600){
-        setTime(0);}
+        setTime(0);
+      }
       
-      if(flag == 0)
+      if(flag == 0){
         digitalWrite(buzzer, HIGH);
-      else
+        servo.detach();
+      }
+      else{
+        servo.attach(servoMotor);
         digitalWrite(buzzer, LOW);
+      }
       
       tela();      
     }
@@ -88,12 +103,22 @@ void tela(){
   lcd.setCursor(0,0);
 }
 
+void empurra(){
+  servo.write(90);
+}
+
+void volta(){
+  servo.write(0);
+}
+
 void onOff(){
   on = !on;
   if(!on){
     digitalWrite(ledR,LOW);
+    digitalWrite(buzzer, LOW);
   }
   else{
       digitalWrite(ledR,HIGH);
+      setTime(0);
     } 
 }
